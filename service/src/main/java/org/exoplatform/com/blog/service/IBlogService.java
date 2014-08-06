410094ca59cb31a71bb7a266f5f77ca61bb4e9b4
ToanNH <toannh@exoplatform.com>
2014-08-06 08:04:13 +0700

    update blog archive table

--- service/src/main/java/org/exoplatform/com/blog/service/IBlogService.java
+++ service/src/main/java/org/exoplatform/com/blog/service/IBlogService.java
@@ -1,9 +1,11 @@
 package org.exoplatform.com.blog.service;
 
+import org.exoplatform.com.blog.service.entity.BlogArchive;
 import org.exoplatform.com.blog.service.util.BlogArchiveUtil;
 
 import javax.jcr.Node;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by toannh on 8/4/14.
@@ -21,5 +23,5 @@ public interface IBlogService {
    */
   public List<Node> getBlogs(String nodeType, String node_date, int year, String selectOptions) throws Exception;
 
-  public BlogArchiveUtil getBlogArchive();
+  public Map<Integer, BlogArchive> getBlogArchive();
 }
--- service/src/main/java/org/exoplatform/com/blog/service/impl/BlogServiceImpl.java
+++ service/src/main/java/org/exoplatform/com/blog/service/impl/BlogServiceImpl.java
@@ -1,6 +1,7 @@
 package org.exoplatform.com.blog.service.impl;
 
 import org.exoplatform.com.blog.service.IBlogService;
+import org.exoplatform.com.blog.service.entity.BlogArchive;
 import org.exoplatform.com.blog.service.util.BlogArchiveUtil;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.ext.app.SessionProviderService;
@@ -13,51 +14,51 @@ import java.util.*;
  */
 public class BlogServiceImpl extends BaseService implements IBlogService {
 
-  private boolean initData = true;
-  private static String BLOG_NODE = "exo:blog";
+    private boolean initData = true;
+    private static String BLOG_NODE = "exo:blog";
 
-  private BlogArchiveUtil archiveUtil = new BlogArchiveUtil();
+    private BlogArchiveUtil<Integer, Integer> blogArchive = new BlogArchiveUtil<Integer, Integer>();
 
-  public BlogServiceImpl(RepositoryService repoService, SessionProviderService sessionProviderService) {
-    this.repoService = repoService;
-    this.sessionProviderService = sessionProviderService;
+    public BlogServiceImpl(RepositoryService repoService, SessionProviderService sessionProviderService) {
+        this.repoService = repoService;
+        this.sessionProviderService = sessionProviderService;
 
-    initBlogArchive();
-    setInitData(false);
-  }
-
-  @Override
-  public List<Node> getBlogs(String nodeType, String node_date, int year, String selectOptions) throws Exception {
-    return getAllNode(nodeType);
-  }
-
-  @Override
-  public BlogArchiveUtil getBlogArchive() {
-    return archiveUtil;
-  }
-
-  void initBlogArchive() {
-    if (isInitData()) {
-      try {
-        List<Node> allNode = getAllNode(BLOG_NODE);
-        Calendar cal = null;
-        for (Node node : allNode) {
-          cal = node.getProperty("exo:dateCreated").getValue().getDate();
-          int _year = cal.get(Calendar.YEAR);
-          int _month = cal.get(Calendar.MONTH);
-          archiveUtil.add(_year, _month);
-        }
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
+        initBlogArchive();
+        setInitData(false);
     }
-  }
 
-  public boolean isInitData() {
-    return initData;
-  }
+    @Override
+    public List<Node> getBlogs(String nodeType, String node_date, int year, String selectOptions) throws Exception {
+        return getAllNode(nodeType);
+    }
 
-  public void setInitData(boolean initData) {
-    this.initData = initData;
-  }
+    void initBlogArchive() {
+        if (isInitData()) {
+            try {
+                List<Node> allNode = getAllNode(BLOG_NODE);
+                Calendar cal = null;
+                for (Node node : allNode) {
+                    cal = node.getProperty("exo:dateCreated").getValue().getDate();
+                    int _year = cal.get(Calendar.YEAR);
+                    int _month = cal.get(Calendar.MONTH);
+                    blogArchive.add(_year, _month);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public boolean isInitData() {
+        return initData;
+    }
+
+    public void setInitData(boolean initData) {
+        this.initData = initData;
+    }
+
+    @Override
+    public Map<Integer, BlogArchive> getBlogArchive() {
+        return blogArchive.getBlogArchive();
+    }
 }
