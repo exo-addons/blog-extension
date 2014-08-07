8920c005ad83da43b99ae83e72f7462b5bcc6e7a
anhvurz90 <vuna@exoplatform.com>
2014-08-07 18:06:16 +0700

    PLF-5994 add sample for using javascript in template

--- webapp/src/main/webapp/resources/js/blog.js
+++ webapp/src/main/webapp/resources/js/blog.js
@@ -1,3 +1,12 @@
-function checkBlog(){
-    alert('Blog\'s js worked! ');
-}
+(function(ecm_bootstrap, gj, wcm_utils) {
+	function blog() {
+	};
+	blog.prototype.test = function() {
+		alert('Blog\'s js worked! ');
+	};
+
+	eXo.ecm.blog = new blog();
+	
+	return eXo.ecm.blog;
+	//-------------------------------------------------------------------------//
+})(gj);
